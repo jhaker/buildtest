d06fa605a70eab1a96b7c472c65bc46e04a00bf4
jhaker@yahoo.com <jhaker@yahoo.com>
2016-11-21 15:24:42 -0600

    added test proj

---
+++ MvcAppSample/MvcAppSample.Tests/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MvcAppSample.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Go()
+        {
+            var x = 1;
+            var y = 0;
+            Assert.AreEqual(x,y);
+        }
+    }
+}
